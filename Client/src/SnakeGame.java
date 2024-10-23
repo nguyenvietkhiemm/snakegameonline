f65790f4e701774a022778fc17e50f4cdc5c7318
nguyenvietkhiemm <nguyenvietkhiemm@gmail.com>
2024-10-23 10:34:42 +0700

    fixed4

--- Client/src/SnakeGame.java
+++ Client/src/SnakeGame.java
@@ -38,9 +38,10 @@ public class SnakeGame extends JPanel implements ActionListener, MouseMotionList
 
     private int viewportX = 0; // Tọa độ góc trên bên trái của viewport
     private int viewportY = 0;
-    private Image backgroundImage; //ảnh nền
+    private Image backgroundImage; // ảnh nền
     Random rand = new Random();
-    private Color snakeColor = new Color(rand.nextFloat(),rand.nextFloat() , rand.nextFloat()); //  tạo màu cố định cho rắn khi bắt đầu game
+    private Color snakeColor = new Color(rand.nextFloat(), rand.nextFloat(), rand.nextFloat()); // tạo màu cố định cho
+                                                                                                // rắn khi bắt đầu game
 
     Gson gson = new Gson();
 
@@ -50,10 +51,14 @@ public class SnakeGame extends JPanel implements ActionListener, MouseMotionList
         setFocusable(true);
         addMouseMotionListener(this);
 
-        //tải ảnh từ đường dẫn
+        // tải ảnh từ đường dẫn
 
         try {
-            backgroundImage = ImageIO.read(new File("C://Users//ADMIN//OneDrive//Desktop//snakegameonline//Client//GameVisual//background.jpg")); // Đường dẫn ảnh nền
+            backgroundImage = ImageIO.read(new File(
+                    "C://Users//ADMIN//OneDrive//Desktop//snakegameonline//Client//GameVisual//background.jpg")); // Đường
+                                                                                                                  // dẫn
+                                                                                                                  // ảnh
+                                                                                                                  // nền
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -87,12 +92,11 @@ public class SnakeGame extends JPanel implements ActionListener, MouseMotionList
         System.out.println(id);
     }
 
-
     public class Food {
-        public Point position;  // vị trí của food
-        public int baseSize;  // Kích thước Food cơ bản
-        public int currentSize;  // Kích thước Food hiện tại để nhấp nháy
-        public Color color;     // màu sắc của food
+        public Point position; // vị trí của food
+        public int baseSize; // Kích thước Food cơ bản
+        public int currentSize; // Kích thước Food hiện tại để nhấp nháy
+        public Color color; // màu sắc của food
         public Color glowColor; // Màu phát sáng (hiệu ứng)
         private boolean increasing = true; // điều khiển đốm sáng nhấm nháy
 
@@ -103,12 +107,12 @@ public class SnakeGame extends JPanel implements ActionListener, MouseMotionList
             this.color = color;
             this.glowColor = glowColor;
         }
-        private int blinkSpeed = 1; // Tốc độ nhấp nháy
 
+        private int blinkSpeed = 1; // Tốc độ nhấp nháy
 
         public void updateBlink() {
             if (increasing) {
-                currentSize += blinkSpeed; //tăng kích thước theo tốc độ nhấp nháy
+                currentSize += blinkSpeed; // tăng kích thước theo tốc độ nhấp nháy
                 if (currentSize >= baseSize + 5) { // Đốm sáng tăng lên tối đa 5 đơn vị
                     increasing = false;
                 }
@@ -122,74 +126,75 @@ public class SnakeGame extends JPanel implements ActionListener, MouseMotionList
 
     }
 
+    // public class Food {
+    // public Point position; // vị trí của food
+    // public int baseSize; // Kích thước Food cơ bản
+    // public int currentSize; // Kích thước Food hiện tại để nhấp nháy
+    // public Color color; // màu sắc của food
+    // public BufferedImage glowImage; // Hình ảnh phát sáng (hiệu ứng blur)
+    // private boolean increasing = true; // điều khiển đốm sáng nhấp nháy
+    // private int blinkSpeed = 1; // Tốc độ nhấp nháy
 
-// public class Food {
-//     public Point position;  // vị trí của food
-//     public int baseSize;  // Kích thước Food cơ bản
-//     public int currentSize;  // Kích thước Food hiện tại để nhấp nháy
-//     public Color color;     // màu sắc của food
-//     public BufferedImage glowImage; // Hình ảnh phát sáng (hiệu ứng blur)
-//     private boolean increasing = true; // điều khiển đốm sáng nhấp nháy
-//     private int blinkSpeed = 1; // Tốc độ nhấp nháy
+    // // Constructor mới, thay glowColor bằng glowImage (hình ảnh với hiệu ứng
+    // blur)
+    // public Food(Point position, int size, Color color, BufferedImage glowImage) {
+    // this.position = position;
+    // this.baseSize = size;
+    // this.currentSize = size;
+    // this.color = color;
+    // this.glowImage = glowImage;
+    // }
 
-//     // Constructor mới, thay glowColor bằng glowImage (hình ảnh với hiệu ứng blur)
-//     public Food(Point position, int size, Color color, BufferedImage glowImage) {
-//         this.position = position;
-//         this.baseSize = size;
-//         this.currentSize = size;
-//         this.color = color;
-//         this.glowImage = glowImage;
-//     }
+    // // Hàm cập nhật hiệu ứng nhấp nháy
+    // public void updateBlink() {
+    // if (increasing) {
+    // currentSize += blinkSpeed; //tăng kích thước theo tốc độ nhấp nháy
+    // if (currentSize >= baseSize + 5) { // Đốm sáng tăng lên tối đa 5 đơn vị
+    // increasing = false;
+    // }
+    // } else {
+    // currentSize -= blinkSpeed;
+    // if (currentSize <= baseSize - 5) { // Đốm sáng giảm xuống tối đa 5 đơn vị
+    // increasing = true;
+    // }
+    // }
+    // }
 
-//     // Hàm cập nhật hiệu ứng nhấp nháy
-//     public void updateBlink() {
-//         if (increasing) {
-//             currentSize += blinkSpeed; //tăng kích thước theo tốc độ nhấp nháy
-//             if (currentSize >= baseSize + 5) { // Đốm sáng tăng lên tối đa 5 đơn vị
-//                 increasing = false;
-//             }
-//         } else {
-//             currentSize -= blinkSpeed;
-//             if (currentSize <= baseSize - 5) { // Đốm sáng giảm xuống tối đa 5 đơn vị
-//                 increasing = true;
-//             }
-//         }
-//     }
+    // // Hàm vẽ Food lên màn hình, vẽ cả food và glowImage (hình ảnh phát sáng)
+    // public void draw(Graphics2D g) {
+    // // Vẽ hình chính (food)
+    // g.setColor(color);
+    // g.fillOval(position.x, position.y, currentSize, currentSize);
 
-//     // Hàm vẽ Food lên màn hình, vẽ cả food và glowImage (hình ảnh phát sáng)
-//     public void draw(Graphics2D g) {
-//         // Vẽ hình chính (food)
-//         g.setColor(color);
-//         g.fillOval(position.x, position.y, currentSize, currentSize);
-
-//         // Vẽ hình phát sáng mờ (glowImage)
-//         int glowSize = currentSize + 10; // Kích thước của đốm sáng lớn hơn một chút
-//         g.drawImage(glowImage, position.x - 5, position.y - 5, glowSize, glowSize, null);
-//     }
-// }
-
-    
+    // // Vẽ hình phát sáng mờ (glowImage)
+    // int glowSize = currentSize + 10; // Kích thước của đốm sáng lớn hơn một chút
+    // g.drawImage(glowImage, position.x - 5, position.y - 5, glowSize, glowSize,
+    // null);
+    // }
+    // }
 
     // private BufferedImage createBlurredImage(int size, Color color) {
-    //     // Tạo BufferedImage để vẽ food
-    //     BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB);
-    //     Graphics2D g2d = image.createGraphics();
+    // // Tạo BufferedImage để vẽ food
+    // BufferedImage image = new BufferedImage(size, size,
+    // BufferedImage.TYPE_INT_ARGB);
+    // Graphics2D g2d = image.createGraphics();
 
-    //     // Vẽ hình tròn với màu food
-    //     g2d.setColor(color);
-    //     g2d.fillOval(0, 0, size, size);
+    // // Vẽ hình tròn với màu food
+    // g2d.setColor(color);
+    // g2d.fillOval(0, 0, size, size);
 
-    //     // Áp dụng Gaussian blur
-    //     float[] kernel = {
-    //             1f / 16f, 2f / 16f, 1f / 16f,
-    //             2f / 16f, 4f / 16f, 2f / 16f,
-    //             1f / 16f, 2f / 16f, 1f / 16f
-    //     };
-    //     ConvolveOp blur = new ConvolveOp(new Kernel(3, 3, kernel), ConvolveOp.EDGE_NO_OP, null);
-    //     BufferedImage blurredImage = blur.filter(image, null);
+    // // Áp dụng Gaussian blur
+    // float[] kernel = {
+    // 1f / 16f, 2f / 16f, 1f / 16f,
+    // 2f / 16f, 4f / 16f, 2f / 16f,
+    // 1f / 16f, 2f / 16f, 1f / 16f
+    // };
+    // ConvolveOp blur = new ConvolveOp(new Kernel(3, 3, kernel),
+    // ConvolveOp.EDGE_NO_OP, null);
+    // BufferedImage blurredImage = blur.filter(image, null);
 
-    //     g2d.dispose();
-    //     return blurredImage;
+    // g2d.dispose();
+    // return blurredImage;
     // }
     public void spawnFood(int numberOfFoods) {
         Random rand = new Random();
@@ -276,64 +281,60 @@ public class SnakeGame extends JPanel implements ActionListener, MouseMotionList
         }
     }
 
-
     public void drawSnake(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); // Kích hoạt chống răng cưa
-    
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); // Kích hoạt chống
+                                                                                                  // răng cưa
+
         int outlineThickness = 3; // Độ dày của viền xung quanh rắn
-    
+
         // Vẽ toàn bộ thân của rắn
         for (int i = snake.size() - 1; i >= 0; i--) { // Vẽ từ phần đuôi lên đầu
             Point p = snake.get(i);
-    
+
             // Vẽ viền mờ quanh các khúc rắn
             g2d.setColor(new Color(0, 0, 0, 50)); // Viền đen mờ
-            g2d.fillOval(p.x - viewportX - outlineThickness, p.y - viewportY - outlineThickness, 
-                         DOT_SIZE + 2 * outlineThickness, DOT_SIZE + 2 * outlineThickness);
-    
+            g2d.fillOval(p.x - viewportX - outlineThickness, p.y - viewportY - outlineThickness,
+                    DOT_SIZE + 2 * outlineThickness, DOT_SIZE + 2 * outlineThickness);
+
             // Vẽ phần thân chính của rắn bên trong viền
             g2d.setColor(snakeColor);
             g2d.fillOval(p.x - viewportX, p.y - viewportY, DOT_SIZE, DOT_SIZE);
         }
-    
+
         // Vẽ mắt trên phần đầu của rắn
         Point head = snake.get(0); // Lấy phần đầu rắn (khúc cao nhất)
         double deltaX = mousePosition.x + viewportX - head.x;
         double deltaY = mousePosition.y + viewportY - head.y;
         double angle = Math.atan2(deltaY, deltaX);
-    
+
         // Điều chỉnh vị trí mắt sao cho mắt luôn nằm trong thân khúc đầu tiên
         int eyeDistance = 8; // Khoảng cách từ giữa thân đến mắt, đảm bảo không vượt viền
         int eyeSize = 10; // Kích thước mắt
-    
+
         // Tính toán vị trí của mắt trái và mắt phải, đảm bảo nằm trong viền
         int eyeOffsetX1 = (int) (Math.cos(angle - Math.PI / 6) * eyeDistance);
         int eyeOffsetY1 = (int) (Math.sin(angle - Math.PI / 6) * eyeDistance);
         int eyeOffsetX2 = (int) (Math.cos(angle + Math.PI / 6) * eyeDistance);
         int eyeOffsetY2 = (int) (Math.sin(angle + Math.PI / 6) * eyeDistance);
-    
+
         // Vẽ mắt trắng bên trong thân đầu tiên
         g2d.setColor(Color.WHITE);
         g2d.fillOval(head.x + eyeOffsetX1 - viewportX, head.y + eyeOffsetY1 - viewportY, eyeSize, eyeSize); // Mắt trái
         g2d.fillOval(head.x + eyeOffsetX2 - viewportX, head.y + eyeOffsetY2 - viewportY, eyeSize, eyeSize); // Mắt phải
-    
+
         // Vẽ con ngươi đen bên trong mắt trắng
         g2d.setColor(Color.BLACK);
-        g2d.fillOval(head.x + eyeOffsetX1 + 2 - viewportX, head.y + eyeOffsetY1 + 2 - viewportY, 5, 5); // Con ngươi trái
-        g2d.fillOval(head.x + eyeOffsetX2 + 2 - viewportX, head.y + eyeOffsetY2 + 2 - viewportY, 5, 5); // Con ngươi phải
-    
+        g2d.fillOval(head.x + eyeOffsetX1 + 2 - viewportX, head.y + eyeOffsetY1 + 2 - viewportY, 5, 5); // Con ngươi
+                                                                                                        // trái
+        g2d.fillOval(head.x + eyeOffsetX2 + 2 - viewportX, head.y + eyeOffsetY2 + 2 - viewportY, 5, 5); // Con ngươi
+                                                                                                        // phải
+
         // Vẽ tên người chơi trên đầu rắn với màu trắng
-        g2d.setColor(Color.WHITE); 
+        g2d.setColor(Color.WHITE);
         g2d.setFont(new Font("Times New Roman", Font.BOLD, 20));
         g2d.drawString(playerName, head.x - viewportX, head.y - 20 - viewportY);
     }
-    
-    
-    
-    
-    
-
 
     private double speedMultiplier = 0.35;
 
@@ -399,7 +400,6 @@ public class SnakeGame extends JPanel implements ActionListener, MouseMotionList
         Point head = snake.get(0);
 
         // Kiểm tra va chạm với bản thân
-       
 
         // Kiểm tra va chạm với biên
         if (head.x < 0 || head.x >= MAP_SIZE || head.y < 0 || head.y >= MAP_SIZE) {
@@ -414,19 +414,18 @@ public class SnakeGame extends JPanel implements ActionListener, MouseMotionList
     public void updateViewport() {
         // Giữ rắn ở trung tâm khung nhìn (viewport)
         Point head = snake.get(0);
-    
+
         // Tỉ lệ để khung nhìn di chuyển chậm, càng nhỏ càng chậm
         double smoothFactor = 0.05;
-    
+
         // Di chuyển từ từ đến vị trí của đầu rắn
         viewportX += (head.x - WIDTH / 2 - viewportX) * smoothFactor;
         viewportY += (head.y - HEIGHT / 2 - viewportY) * smoothFactor;
-    
+
         // Đảm bảo khung nhìn không ra khỏi bản đồ
         viewportX = Math.max(0, Math.min(viewportX, MAP_SIZE - WIDTH));
         viewportY = Math.max(0, Math.min(viewportY, MAP_SIZE - HEIGHT));
     }
-    
 
     public void showGameOver(Graphics g) {
         g.setColor(Color.WHITE);
@@ -454,11 +453,11 @@ public class SnakeGame extends JPanel implements ActionListener, MouseMotionList
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                
+
                 // Vẽ nền gradient
                 Graphics2D g2d = (Graphics2D) g;
                 Color color1 = new Color(18, 18, 18); // màu nền tối
-                Color color2 = new Color(0, 0, 0); 
+                Color color2 = new Color(0, 0, 0);
                 GradientPaint gp = new GradientPaint(0, 0, color1, getWidth(), getHeight(), color2);
                 g2d.setPaint(gp);
                 g2d.fillRect(0, 0, getWidth(), getHeight());
@@ -472,7 +471,7 @@ public class SnakeGame extends JPanel implements ActionListener, MouseMotionList
         titleLabel.setFont(new Font("Arial", Font.BOLD, 40));
         titleLabel.setForeground(new Color(0, 255, 0)); // màu gradient xanh
         titleLabel.setOpaque(false);
-        
+
         // Hiệu ứng Gradient cho chữ
         GradientPaint gradient = new GradientPaint(0, 0, new Color(0, 255, 0), 300, 0, new Color(128, 0, 128));
         titleLabel.setForeground(new Color(0, 255, 0));
@@ -486,7 +485,7 @@ public class SnakeGame extends JPanel implements ActionListener, MouseMotionList
         nameField.setCaretColor(Color.WHITE); // con trỏ trắng
         nameField.setHorizontalAlignment(JTextField.CENTER);
         nameField.setBorder(BorderFactory.createLineBorder(new Color(150, 75, 0), 2)); // Viền màu nâu
-        
+
         // Nút Play
         JButton playButton = new JButton("Play");
         playButton.setBackground(new Color(60, 180, 75));
@@ -532,9 +531,7 @@ public class SnakeGame extends JPanel implements ActionListener, MouseMotionList
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null); // căn giữa màn hình
         frame.setVisible(true);
-        
-    
-    }
 
+    }
 
 }
